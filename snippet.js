f314a0ee1c9e3f0fba84c8cbae9de5c46862bdfa
satanbaby <30554080+satanbaby@users.noreply.github.com>
2020-04-09 23:58:58 +0800

    aggregate

--- snippet.js
+++ snippet.js
@@ -4,3 +4,13 @@ dataSource.fetch(function(){
   console.log(data.length);  // displays "77"
   console.log(data[0].ProductName); // displays "Chai"
 });
+
+// DataSource
+// "average" - Only for Number.
+// "count" - String, Number and Date.
+// "max" - Number and Date.
+// "min" - Number and Date.
+// "sum" - Only for Number.
+aggregate: [
+  { field: "age", aggregate: "sum" }
+]
